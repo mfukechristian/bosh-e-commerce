3f4b3fb6b415417c962f8382ab6a64fe059c8a61
mfukechristian <mfukechristian@gmail.com>
2024-12-22 20:01:10 +0200

    get product from backend

--- backend/data/products.js
+++ backend/data/products.js
@@ -14,7 +14,7 @@ const products = [
   },
   {
     _id: "2",
-    name: "iPhone 11 Pro 256GB Memory",
+    name: "iPhone 13 Pro 256GB Memory",
     image: "/images/phone.jpg",
     description:
       "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
@@ -40,7 +40,7 @@ const products = [
   },
   {
     _id: "4",
-    name: "Sony Playstation 4 Pro White Version",
+    name: "Sony Playstation 5 Pro White Version",
     image: "/images/playstation.jpg",
     description:
       "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
