4cd299ce3dc104b74cbf4269545755ad038fb6c7
mfukechristian <mfukechristian@gmail.com>
2024-12-22 19:17:30 +0200

    add router to navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-function Header() {
+const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            ProShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <Nav.Link as={Link} to="/cart">
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+              <Nav.Link as={Link} to="/login">
                 <FaUser /> Sign In
               </Nav.Link>
             </Nav>
@@ -23,6 +26,6 @@ function Header() {
       </Navbar>
     </header>
   );
-}
+};
 
 export default Header;
